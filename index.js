8846dcd3fb657beab858037aecad51677a826ac9
Pablo <91420916+robot-albatross@users.noreply.github.com>
2023-02-21 11:43:15 -0300

    Feat. Add user input for the square

--- index.js
+++ index.js
@@ -7,24 +7,53 @@
 //     document.body.appendChild(div);
 // }
 
-const BOXWIDTH = 400;
-const USERSQUARES = 8;
-const TOTALSQUARES = BOXWIDTH/USERSQUARES; 
+const BOXWIDTH = 400; //this has to be the same as the css bigBox dmension
+let USERSQUARES = 3; //default value
+let TOTALSQUARES = BOXWIDTH/USERSQUARES; 
 
-let bigBox = document.createElement("div");
-bigBox.classList.add("bigBox");
-document.body.appendChild(bigBox);
+function makeSquare(USERSQUARES) {
 
-for (let i = 1; i <= USERSQUARES**2; i++) {
-    let littleBox = document.createElement("div");
-    littleBox.classList.add("littleBox");
+    let TOTALSQUARES = BOXWIDTH/USERSQUARES;
 
-    littleBox.style.width = TOTALSQUARES + "px";
-    littleBox.style.height = TOTALSQUARES + "px";
+    let bigBox = document.createElement("div");
+    bigBox.classList.add("bigBox");
+    document.body.appendChild(bigBox);
 
-    littleBox.addEventListener("mouseenter",function eventHandler(e) {
-        littleBox.style.backgroundColor = "rgb(" + Math.random()*300 + "," + Math.random()*300 + "," + Math.random()*300+")";;
-    });
+    for (let i = 1; i <= USERSQUARES**2; i++) {
+        let littleBox = document.createElement("div");
+        littleBox.classList.add("littleBox");
 
-    bigBox.appendChild(littleBox);
+        littleBox.style.width = TOTALSQUARES + "px";
+        littleBox.style.height = TOTALSQUARES + "px";
+
+        littleBox.addEventListener("mouseenter",function eventHandler(e) {
+            littleBox.style.backgroundColor = "rgb(" + Math.random()*300 + "," + Math.random()*300 + "," + Math.random()*300+")";;
+        });
+
+        bigBox.appendChild(littleBox);
+    } 
 }
+
+function deleteSquare() {
+    if (document.querySelector(".bigBox")) {
+        bigBox = document.querySelector(".bigBox");
+        document.body.removeChild(bigBox);   
+    }
+}
+
+makeSquare(USERSQUARES);
+
+form = document.querySelector("#form");
+// for (let i = 0; i < form.length; i++) {
+//     console.log(form.elements[i].value);
+// }
+
+button = document.querySelector("#output");
+button.addEventListener("click", function eventHandler (e) {
+    deleteSquare();
+    console.log(form.elements[0].value);
+    USERSQUARES = form.elements[0].value;
+    makeSquare(USERSQUARES);
+});
+
+
